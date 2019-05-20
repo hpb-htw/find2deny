57d304defd4fca1299fd7f2318b7f0897294bcb3
Hong-Phuc Bui <hong-phuc.bui@htwsaar.de>
2019-05-20 15:56:21 +0200

    test-release OK at

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,32 +3,6 @@ import pprint
 from find2deny import cli
 
 
-def test_parse_argv():
-    argv = ["--verbosity", "DEBUG", "test-data/rules.cfg"]
-    cli_config = cli.parse_arg(argv)
-    effective_config = cli.merge_config(cli_config)
-
-    assert effective_config["verbosity"] == "DEBUG"
-    assert effective_config["log_files"][0] == "test-data/apache2/access.log.8"
-    assert effective_config["log_pattern"] == '%h %l %u %t "%r" %>s %O "%{Referer}i" "%{User-Agent}i"'
-    assert effective_config["judgments_chain"] == ["path-based-judgment", "time-based-judgment"]
-    assert len(effective_config["bot_request"]) >= 14
-    assert effective_config["max_request"] == 501
-    assert effective_config["interval_seconds"] == 59
-
-
-def test_parse_argv_no_option():
-    argv = ["test-data/rules.cfg"]
-    cli_config = cli.parse_arg(argv)
-    effective_config = cli.merge_config(cli_config)
-    assert effective_config["verbosity"] == "INFO"
-    assert effective_config["log_files"][0] == "test-data/apache2/access.log.8"
-    assert effective_config["log_pattern"] == '%h %l %u %t "%r" %>s %O "%{Referer}i" "%{User-Agent}i"'
-    assert effective_config["judgments_chain"] == ["path-based-judgment", "time-based-judgment"]
-    assert len(effective_config["bot_request"]) >= 14
-    assert effective_config["max_request"] == 501
-    assert effective_config["interval_seconds"] == 59
-
 
 def test_expand_logfiles():
     blob_logs = ["test-data/apache2/*.gz"]
@@ -37,10 +11,16 @@ def test_expand_logfiles():
 
 
 def test_construct_judgment():
-    config = {"bot_request":["/phpmyadmin.php"]}
-    name = "path-based-judgment"
-    judgment = cli.judgment_by_name(name, config)
-    assert str(judgment).count("/phpmyadmin.php") >= 1
+    judge = {
+        "name": "path-based-judgment",
+        "rules": {
+            "bot_request":[
+                "/phpMyAdmin.php",
+            ]
+        }
+    }
+    judgment = cli.judgment_by_name(judge, None)
+    assert str(judgment).count("/phpMyAdmin.php") >= 1
 
 
 
--- tests/test_config_parser.py
+++ tests/test_config_parser.py
@@ -6,5 +6,5 @@ import pprint
 def test_parse_config_file():
     test_config_file = "test-data/rules.toml"
     config = config_parser.parse_config_file(test_config_file)
-    print("\n")
-    pprint.pprint(config)
+    assert len(config['judgment']) == 2
+    assert config['judgment'][0]['name'] == 'path-based-judgment'
